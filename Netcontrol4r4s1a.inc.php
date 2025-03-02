abcd19fcceaee3afce353496fbf03036a9c9cfe3
Dimitar Minekov <dminekov@abv.bg>
2025-03-02 12:14:26 +0200

    Added port addresses

--- Netcontrol4r4s1a.inc.php
+++ Netcontrol4r4s1a.inc.php
@@ -12,33 +12,46 @@ class Netcontrol4r4s1a
     function __construct()
     {
         $this->registers = [
-            'Sensor1' => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'no'],
-            'Sensor2' => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'no'],
-            'Sensor3' => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'no'],
-            'Sensor4' => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'no'],
-            'Alarm'   => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'no'],
-            'Relay1'  => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'yes'],
-            'Relay2'  => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'yes'],
-            'Relay3'  => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'yes'],
-            'Relay4'  => ['title' => '', 'measure' => '', 'address' => '', 'readable' => 'yes', 'writable' => 'yes'],
+            'Sensor1' => ['title' => 'T1', 'measure' => 'Temperature', 'address' => '25', 'access' => 'r'],
+            'Sensor2' => ['title' => 'Hr', 'measure' => 'Humidity', 'address' => '28', 'access' => 'r'],
+            'Sensor3' => ['title' => 'T2', 'measure' => 'Temperature', 'address' => '29', 'access' => 'r'],
+            'Sensor4' => ['title' => '', 'measure' => '', 'address' => '', 'access' => 'r'],
+            'Alarm'   => ['title' => '', 'measure' => '', 'address' => '', 'access' => 'r'],
+            'Relay1'  => ['title' => '', 'measure' => '', 'address' => '9', 'access' => 'rw'],
+            'Relay2'  => ['title' => '', 'measure' => '', 'address' => '10', 'access' => 'rw'],
+            'Relay3'  => ['title' => '', 'measure' => '', 'address' => '11', 'access' => 'rw'],
+            'Relay4'  => ['title' => '', 'measure' => '', 'address' => '12', 'access' => 'rw'],
         ];
     }
-
-    function init($ip, $port = 80, $user = 'admin', $pass = 'admin')
+    function fetchUrl($url)
     {
-        $url = "http://" . $ip . ":" . $port . "/iocfg.js"; // die($url);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, "$user:$pass");
+        curl_setopt($ch, CURLOPT_USERPWD, "$this->user:$this->pass");
 
         if (($response = curl_exec($ch)) === false) {
             $response = curl_error($ch);
         }
         curl_close($ch);
+
+        return $response;
+    }
+
+
+
+    function init($ip, $port = 80, $user = 'admin', $pass = 'admin')
+    {
+        $this->user = $user;
+        $this->pass = $pass;
+        
+        $urlRegs = "http://" . $ip . ":" . $port . "/iocfg.js";
+        $urlRes  = "http://" . $ip . ":" . $port . "/iocfg.js"; // die($url);
+      
+        $response = $this->fetchUrl($urlRegs);
 
         $this->config = $response;
     }
--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include 'Netcontrol4r4s1a.inc.php';
 
 $controller = new Netcontrol4r4s1a();
 
-// $controller->init('192.168.1.100');
-$controller->init('192.168.100.80');
+ $controller->init('192.168.1.100');
+//$controller->init('192.168.100.80');
 
 echo $controller->showConfig();
