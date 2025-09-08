825bc2b186ca48d8efd6f7e75cd4f9ecf7ebc1f9
Dimitar Minekov <dminekov@abv.bg>
2025-09-08 16:34:56 +0300

    Read and write functions

--- Netcontrol4r4s1a.inc.php
+++ Netcontrol4r4s1a.inc.php
@@ -7,23 +7,25 @@ class Netcontrol4r4s1a
     private $pass;
 
     private $registers = [];
-    private $config;
+    private $data;
+    private $urlIOChange;
+    private $error;
 
     function __construct()
     {
         $this->registers = [
-            'Sensor1' => ['title' => 'T1', 'measure' => 'Temperature', 'address' => '25', 'access' => 'r'],
-            'Sensor2' => ['title' => 'Hr', 'measure' => 'Humidity', 'address' => '28', 'access' => 'r'],
-            'Sensor3' => ['title' => 'T2', 'measure' => 'Temperature', 'address' => '29', 'access' => 'r'],
-            'Sensor4' => ['title' => '', 'measure' => '', 'address' => '', 'access' => 'r'],
-            'Alarm'   => ['title' => '', 'measure' => '', 'address' => '', 'access' => 'r'],
-            'Relay1'  => ['title' => '', 'measure' => '', 'address' => '9', 'access' => 'rw'],
-            'Relay2'  => ['title' => '', 'measure' => '', 'address' => '10', 'access' => 'rw'],
-            'Relay3'  => ['title' => '', 'measure' => '', 'address' => '11', 'access' => 'rw'],
-            'Relay4'  => ['title' => '', 'measure' => '', 'address' => '12', 'access' => 'rw'],
+            'Sensor1' => ['title' => 'T1', 'measure' => 'Temperature', 'address' => '24', 'access' => 'r'],
+            'Sensor2' => ['title' => 'Hr', 'measure' => 'Humidity', 'address' => '27', 'access' => 'r'],
+            'Sensor3' => ['title' => 'T2', 'measure' => 'Temperature', 'address' => '28', 'access' => 'r'],
+            'Sensor4' => ['title' => 'Sensor4', 'measure' => '', 'address' => '29', 'access' => 'r'],
+            'Alarm'   => ['title' => 'Alarm', 'measure' => '', 'address' => '30', 'access' => 'r'],
+            'Relay1'  => ['title' => 'Relay1', 'measure' => '', 'address' => '8', 'access' => 'rw'],
+            'Relay2'  => ['title' => 'Relay2', 'measure' => '', 'address' => '9', 'access' => 'rw'],
+            'Relay3'  => ['title' => 'Relay3', 'measure' => '', 'address' => '10', 'access' => 'rw'],
+            'Relay4'  => ['title' => 'Relay4', 'measure' => '', 'address' => '11', 'access' => 'rw'],
         ];
     }
-    function fetchUrl($url)
+    private function fetchUrl($url)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -34,7 +36,7 @@ class Netcontrol4r4s1a
         curl_setopt($ch, CURLOPT_USERPWD, "$this->user:$this->pass");
 
         if (($response = curl_exec($ch)) === false) {
-            $response = curl_error($ch);
+            $this->error = curl_error($ch);
         }
         curl_close($ch);
 
@@ -43,26 +45,136 @@ class Netcontrol4r4s1a
 
 
 
-    function init($ip, $port = 80, $user = 'admin', $pass = 'admin')
+    public function init($ip, $port = 80, $user = 'admin', $pass = 'admin')
     {
         $this->user = $user;
         $this->pass = $pass;
+        $this->ip = $ip;
+        $this->port = $port;
+
+        $this->urlIOChange = 'http://'.$ip.'/iochange.cgi?ref=re-io';
         
-        $urlRegs = "http://" . $ip . ":" . $port . "/iocfg.js";
-        $urlRes  = "http://" . $ip . ":" . $port . "/iocfg.js"; // die($url);
-      
-        $response = $this->fetchUrl($urlRegs);
+        $urlConf = "http://" . $this->ip . ":" . $this->port . "/ioreg.js";
+        if (false === ($data = $this->fetchUrl($urlConf))) {
+            return false; // $this->error
+        }   
 
-        $this->config = $response;
+        $SHT = $this->extractArray($data, 'SHT');
+        $IO  = $this->extractArray($data, 'IO');
+        $PM  = $this->extractArray($data, 'PM');
+ 
+        // Преобразуваме към десетични числа
+        $this->data['SHT'] = array_map(fn($h)=>hexdec($h), $SHT); 
+        $this->data['IO']  = array_map(fn($h)=>hexdec($h), $IO);
+        $this->data['PM']  = array_map(fn($h)=>hexdec($h), $PM);
     }
 
-    function readRegister() {}
-
-    function writeRegister() {}
-
-    public function showConfig()
+    public function readRegisters()
     {
+        foreach ($this->registers as $name => $reg) {
+            $addr = (int)$reg['address'];
+            $pm_code = $this->data['PM'][$addr];
+            $v_raw = $this->data['IO'][$addr];
+            $SHT0 = $this->data['SHT'][0];
+            $SHT1 = $this->data['SHT'][1];
 
-        return $this->config;
+            $value = $this->fcValue($pm_code, $v_raw, $SHT0, $SHT1);
+            if ($name === 'Alarm') {
+                // Специално за алармата
+                $value = ($value > 511) ? "OPEN" : "CLOSED";
+            }
+
+             if (false !== strpos($name,'Relay')) {
+                $this->data['sens'][$name] = $value ? "ON" : "OFF";
+            } else {
+                $this->data['sens'][$name] = $value;
+            }
+        }
+
+        return $this->data['sens'];
     }
+
+    public function writeRegisters($name, $value)
+    {
+        if (isset($this->registers[$name] ) && strpos($this->registers[$name]['access'], 'w') !== false ) {
+            // Create a stream
+            $opts = [
+                'http'=>[
+                    'method'=>"GET",
+                    'header' => "Authorization: Basic " . base64_encode("{$this->user}:{$this->pass}") . "\r\n"               
+                ]
+            ];
+            $context = stream_context_create($opts);
+            $addr = $this->registers[$name]['address'];
+            $cmd = sprintf("%02X=%02X",intval($addr),intval($value));
+            
+            // Open the file using the HTTP headers set above
+            $file = file_get_contents($this->urlIOChange.'&'.$cmd, false, $context);
+
+            return true;
+        }
+        return false;
+    }
+
+    // Функция за извличане на масиви от скрипта
+    private function extractArray($script, $varName, $delimiter = ',') {
+        $lines = explode("\n", $script);
+    
+        foreach ($lines as $line) {
+            $line = trim($line);
+            
+            if (strpos($line, "var $varName=") === 0) {
+                $value = substr($line, strlen("var $varName=")); // Премахване на "var IO="
+                $value = trim($value, " ;"); // Премахване на ";"
+    
+                // Проверяваме дали масивът е в кавички или в квадратни скоби
+                if ($value[0] === '"') {
+                    $value = trim($value, '"'); // Премахваме кавичките
+                } elseif ($value[0] === '[') {
+                    $value = trim($value, '[]'); // Премахваме квадратните скоби
+                }
+    
+                return array_map('trim', explode($delimiter, $value)); // Разделяме по запетая
+            }
+        }
+        return [];
+    }
+
+    // === Реализация на lib.js::fc(i,v) за нужните случаи ===
+    // v_raw е 10-битов ADC (0..1023) от IO[]; първо става в милитиволти
+    private function fcValue($pm_code, $v_raw, $SHT0, $SHT1) {
+        // 10-bit ADC @ 3.3V
+        $v = 3300.0 * ($v_raw & 0x3FF) / 1023.0; // mV
+
+        switch ($pm_code) {
+            case 33: // case 33: v=(v-500)/10; q=1  -> °C (TMP36-подобно)
+                return round(($v - 500.0) / 10.0, 1);
+
+            case 39: // SHT temp: v=-(SHT0/SHT1)*(v-1650)/22/1000; q=2
+                $coef = $SHT0 / $SHT1;
+                return round(-$coef * ($v - 1650.0) / (22.0 * 1000.0), 2);
+
+            case 40: // Humidity: v=(125*v/3300)-6; q=0
+                $rh = (125.0 * $v / 3300.0) - 6.0;
+                if ($rh < 0) $rh = 0;
+                if ($rh > 100) $rh = 100;
+                return round($rh, 0);
+
+            case 38: // Raw ADC count (0..1023)
+                // fc прави v=v*1023/3300, което се връща към суров count
+                $cnt = $v * 1023.0 / 3300.0;
+                return (int)round($cnt);
+
+            case 0:  // дигитални 0/1
+            case 1:
+            case 2:
+            case 8:
+                return ($v_raw ? 1 : 0);
+
+            default:
+                // по подразбиране върни mV закръглено
+                return (int)round($v);
+        }
+    }
+
 }
--- test.php
+++ test.php
@@ -3,7 +3,14 @@ include 'Netcontrol4r4s1a.inc.php';
 
 $controller = new Netcontrol4r4s1a();
 
- $controller->init('192.168.1.100');
-//$controller->init('192.168.100.80');
+//$controller->init('192.168.1.100');
+$controller->init('192.168.100.80');
 
-echo $controller->showConfig();
+$sens = $controller->readRegisters();
+print_r($sens);
+
+
+$controller->writeRegisters('Relay1', 0);
+$controller->writeRegisters('Relay2', 0);
+$controller->writeRegisters('Relay3', 0);
+$controller->writeRegisters('Relay4', 0);
